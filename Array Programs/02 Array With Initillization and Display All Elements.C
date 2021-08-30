c9bbe0052ff93ade7b7c29219b00855423814cfb
AkashKank <87307240+AkashKank@users.noreply.github.com>
2021-08-31 00:38:09 +0530

    / Array

---
+++ Array Programs/02 Array With Initillization and Display All Elements.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i[5] = {10,20,30,40,50},No;
+
+    for(No = 0; No < 5; No++)
+    {
+        printf("\n Display Elements are : %d.",i[No]);
+    }
+
+    _getch();
+    return 0;
+}
