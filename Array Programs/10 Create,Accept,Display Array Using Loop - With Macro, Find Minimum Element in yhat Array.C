87546b1682ce61b65e99506cd5b2f317381c7e39
AkashKank <87307240+AkashKank@users.noreply.github.com>
2021-09-20 23:48:16 +0530

    Array

---
+++ Array Programs/10 Create,Accept,Display Array Using Loop - With Macro, Find Minimum Element in yhat Array.C
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<conio.h>
+
+#define Size 7
+
+void Accept_Bills(int*);
+void Display_Bills(int*);
+int Find_Min(int*);
+
+int main()
+{
+    int Bills[Size] = {}, Ret = -2;
+
+    Accept_Bills(Bills);
+    Display_Bills(Bills);
+    Ret = Find_Min(Bills);
+
+    printf("\n Minimum Amount in Bill is = %d", Ret);
+
+    _getch();
+    return 0;
+}
+
+int Find_Min(int* ptr)
+{
+    int Min = 0, i = 0;
+
+
+    for( i = 0; i < Size; i++ )
+    {
+        if( (i == 0) || (Min > ptr[i]))
+        {
+            Min = ptr[i];
+        }
+    }
+
+    return Min;
+}
+
+void Accept_Bills(int *ptr)
+{
+    for(int i = 0; i < Size; i++)
+    {
+        printf("\n Enter %d Bill = ", i+1);
+        scanf("%d", &ptr[i]);
+    }
+    return;
+}
+
+void Display_Bills(int *ptr)
+{
+    for(int i = 0; i < Size; i++)
+    {
+        printf("\n Bill No %d = %d",i+1, ptr[i]);
+    }
+    return;
+}
+
+
