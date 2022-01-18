9cefdd0522367f1dc906156f61458748e2cab0f5
AkashKank <87307240+AkashKank@users.noreply.github.com>
2022-01-18 22:51:47 +0530

    /Practice Programs C

---
+++ Practice prog C/Size of Operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n Size OF int Data Type : %d",sizeof(int));
+    printf("\n Size of char Data Type : %d",sizeof(char));
+    printf("\n Size of float Data Type : %d",sizeof(float));
+    printf("\n Size of double Data Type : %d",sizeof(double));
+
+    return 0;
+}
