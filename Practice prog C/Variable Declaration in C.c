8c0d08903eae4b3101fda9ce997c79cc90839b77
AkashKank <87307240+AkashKank@users.noreply.github.com>
2022-01-18 22:56:04 +0530

    /Practice Programs C

---
+++ Practice prog C/Variable Declaration in C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    char c;
+    float f;
+
+    a = 10;
+    b = 20;
+    c = a + b;
+    printf("\n Value of c : %d",c);
+
+    f = 70.0/3.0;
+    printf("\n Value of f : %f",f);
+
+    return 0;
+}
