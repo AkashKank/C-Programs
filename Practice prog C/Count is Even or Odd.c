fa92a83435d981d792b26231fc66a5c3b229263a
AkashKank <87307240+AkashKank@users.noreply.github.com>
2022-02-09 00:05:56 +0530

    /Practice Programs C

---
+++ Practice prog C/Count is Even or Odd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int No;
+
+
+    printf("\n Enter Any Number: ");
+    scanf("%d",&No);
+
+    if(No%2 == 0)
+
+      printf("\n %d Is Even.",No);
+
+
+    else
+
+        printf("\n %d is Odd",No);
+
+    return 0;
+}
+
