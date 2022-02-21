0eab5344f2f9e93e36604ee967b66d162d1fbf3c
AkashKank <87307240+AkashKank@users.noreply.github.com>
2022-02-22 00:03:50 +0530

    /Structures

---
+++ Structures/01 Basics of Structures.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct student
+{
+    int Roll_No;
+    char Name[80];
+    char City[20];
+    float Per;
+};
+
+int main()
+{
+    int i;
+
+    printf("\n Size of given Structure = %d",sizeof(i));
+
+    getch();
+    return 0;
+}
