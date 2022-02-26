d6772cf1472fb6ef77eb28f976dc310a8d2b2536
AkashKank <87307240+AkashKank@users.noreply.github.com>
2022-02-27 00:14:05 +0530

    /Functions

---
+++ Functions/05 Program To Print Table of Inputted Number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Table(int);
+
+int main()
+{
+    int No;
+
+    printf("\n Enter any Number to Print its Table : ");
+    scanf("%d",&No);
+
+    Table(No);
+
+    return 0;
+}
+
+void Table(int No)
+{
+    int i;
+
+    for(i=1;i<=10;i++)
+    {
+        printf("\n%d*%d=%d",No,i,No*i);
+    }
+    return i;
+}
