964a5bc5a7840449d03cc3e9559279f8fb7123d2
AkashKank <87307240+AkashKank@users.noreply.github.com>
2022-03-04 12:07:02 +0530

    /Functions

---
+++ Functions/13 Power Function of Two Numbers using if else.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Power_Function(int,int);
+int main()
+{
+    int No1,No2,Res;
+    printf("\n Enter a Number : ");
+    scanf("%d",&No1);
+    printf("\n Enter a Number : ");
+    scanf("%d",&No2);
+
+    Res = Power_Function(No1,No2);
+
+    printf("\n(%d)^%d = %d",No1,No2,Res);
+    return 0;
+}
+
+int Power_Function(int N1,int N2)
+{
+    int Pow = 1;
+
+    if(N1==1 || N2==0)
+    {
+        goto DWN;
+    }
+    else if(N1==0)
+    {
+        Pow = 0;
+        goto DWN;
+    }
+    else if(N2 == 1)
+    {
+        Pow = N1;
+        goto DWN;
+    }
+    else
+    {
+        while(N2 > 0)
+        {
+            Pow = Pow*N1;
+            N2--;
+        }
+    }
+    DWN:
+    return Pow;
+}
