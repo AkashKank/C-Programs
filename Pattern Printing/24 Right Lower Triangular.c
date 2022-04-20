8e91ce00f40d7eb69c19f626a9986a4395d6a19c
Akash Kank <akashkank4@gmail.com>
2022-04-20 16:10:08 +0530

    Pattern Printing

---
+++ Pattern Printing/24 Right Lower Triangular.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=0,j=0,x=0;
+    printf("Enter No of Rows & Columns for Pattern:");
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+    {
+        for(j=1;j<=x;j++)
+        {
+            if(i+j>x)
+            {
+                printf(" * ");
+            }
+        else
+        {
+            printf("   ");
+        }
+
+        }
+        printf("\n");
+    }
+    _getch();
+    return 0;
+}
