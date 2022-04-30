7c19881dd507faf08628bf4ccc1baf0b9bfa62b0
Akash Kank <akashkank4@gmail.com>
2022-04-30 23:58:36 +0530

    Basic Programs

---
+++ Basic Programs/06 Print Ascii Table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("****Ascii Table****\n");
+
+  for(int i=0;i<128;i++)
+   {
+     printf(" %d =\t%c \n",i,i);
+   }
+
+  getch();
+  return 0;
+ }
