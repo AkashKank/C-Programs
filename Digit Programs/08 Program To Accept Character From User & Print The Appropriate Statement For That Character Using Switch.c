232b5d1d90794676a884e61713687c0832c3970b
Akash Kank <akashkank4@gmail.com>
2022-05-22 21:28:47 +0530

    Digit Programs

---
+++ Digit Programs/08 Program To Accept Character From User & Print The Appropriate Statement For That Character Using Switch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+    char ch;
+
+    printf("Enter Character Between[(A-D)/(a-d)]:");
+    scanf("%c",&ch);
+
+     switch(ch)
+     {
+       case 'A':
+       case 'a':
+               printf("Welcome....");
+               break;
+       case 'B':
+       case 'b':
+               printf("Good Bye...");
+               break;
+       case 'C':
+       case 'c':
+               printf("Have a nice day...");
+               break;
+      case 'D':
+      case 'd':
+              printf("Good Day...");
+              break;
+     default:
+              printf("Thanks...");
+    }
+
+  getch();
+  return 0;
+ }
